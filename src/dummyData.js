f465faf9a698f67c7f802edff9dff661a980a437
Sami <saminazirqureshi1@gmail.com>
2024-05-14 16:06:15 +0500

    complete

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,52 +1,52 @@
 export const Users = [
   {
     id:1,
-    profilePicture: "/src/assets/person/1.jpeg",
+    profilePicture: "/src/assets/Person/1.jpeg",
     username: "Safak Kocaoglu",
   },
   {
     id:2,
-    profilePicture: "/src/assets/person/2.jpeg",
+    profilePicture: "/src/assets/Person/2.jpeg",
     username: "Janell Shrum",
   },
   {
     id:3,
-    profilePicture: "/src/assets/person/3.jpeg",
+    profilePicture: "/src/assets/Person/3.jpeg",
     username: "Alex Durden",
   },
   {
     id:4,
-    profilePicture: "/src/assets/person/4.jpeg",
+    profilePicture: "/src/assets/Person/4.jpeg",
     username: "Dora Hawks",
   },
   {
     id:5,
-    profilePicture: "/src/assets/person/5.jpeg",
+    profilePicture: "/src/assets/Person/5.jpeg",
     username: "Thomas Holden",
   },
   {
     id:6,
-    profilePicture: "/src/assets/person/6.jpeg",
+    profilePicture: "/src/assets/Person/6.jpeg",
     username: "Shirley Beauchamp",
   },
   {
     id:7,
-    profilePicture: "/src/assets/person/7.jpeg",
+    profilePicture: "/src/assets/Person/7.jpeg",
     username: "Travis Bennett",
   },
   {
     id:8,
-    profilePicture: "/src/assets/person/8.jpeg",
+    profilePicture: "/src/assets/Person/8.jpeg",
     username: "Kristen Thomas",
   },
   {
     id:9,
-    profilePicture: "/src/assets/person/9.jpeg",
+    profilePicture: "/src/assets/Person/9.jpeg",
     username: "Gary Duty",
   },
   {
     id:10,
-    profilePicture: "/src/assets/person/4.jpeg",
+    profilePicture: "/src/assets/Person/4.jpeg",
     username: "Safak Kocaoglu",
   },
 ];
